a2b253b5429da1b73c50d2661d39acc86311e97b
Srisivas-appdeveloper <srisivas362@gmail.com>
2025-12-04 19:54:23 +0530

    completed login

---
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -0,0 +1,30 @@
+// Flutter web plugin registrant file.
+//
+// Generated file. Do not edit.
+//
+
+// @dart = 2.13
+// ignore_for_file: type=lint
+
+import 'package:audioplayers_web/audioplayers_web.dart';
+import 'package:connectivity_plus/src/connectivity_plus_web.dart';
+import 'package:flutter_blue_plus_web/flutter_blue_plus_web.dart';
+import 'package:flutter_secure_storage_web/flutter_secure_storage_web.dart';
+import 'package:image_picker_for_web/image_picker_for_web.dart';
+import 'package:package_info_plus/src/package_info_plus_web.dart';
+import 'package:permission_handler_html/permission_handler_html.dart';
+import 'package:record_web/record_web.dart';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+void registerPlugins([final Registrar? pluginRegistrar]) {
+  final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
+  AudioplayersPlugin.registerWith(registrar);
+  ConnectivityPlusWebPlugin.registerWith(registrar);
+  FlutterBluePlusWeb.registerWith(registrar);
+  FlutterSecureStorageWeb.registerWith(registrar);
+  ImagePickerPlugin.registerWith(registrar);
+  PackageInfoPlusWebPlugin.registerWith(registrar);
+  WebPermissionHandler.registerWith(registrar);
+  RecordPluginWeb.registerWith(registrar);
+  registrar.registerMessageHandler();
+}
